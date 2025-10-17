5b4455c8049c4cc8a867d839a3f614abbd43678b
Vincent Zhang <73851560+bootsareme@users.noreply.github.com>
2025-10-17 17:18:41 -0400

    Add utility functions for file reading and 2D printing
    This file contains utility functions for reading files and printing 2D vectors to the console. It includes a function to parse a drawing from a file into a 2D vector format.

---
+++ Util.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <fstream>
+#include <string>
+
+/* Utility functions for reading files and printing data to console. */
+namespace Util
+{
+    /// <summary> Utility function to print 2D vectors. </summary>
+    /// <param name="vector2D"> 2D vector to be printed. </param>
+    template <typename T>
+    void print_2Dvector(const std::vector<std::vector<T>>& vector2D)
+    {
+        for (const std::vector<T>& components : vector2D)
+        {
+            for (T element : components)
+                std::cout << std::right << std::setw(6) << element;
+            std::cout << '\n';
+        }
+    }
+
+    /// <summary>
+    ///     Converts human-readable drawing into 2D vectors.
+    ///     Use "@" for a shaded black pixel, "." for a empty white pixel.
+    ///     Put 1 drawing only, additional drawings will be ignored. Do not go above 9x9.
+    /// </summary>
+    ///     <param name="file"> name of the file </param>
+    ///     <returns> 2D vector of machine-readable format of image </returns>
+    std::vector<std::vector<int>> parse_file(const char* file)
+    {
+        std::ifstream input(file);
+        std::vector<std::vector<int>> singleDrawing;
+
+        for (std::string line; std::getline(input, line);)
+        {
+            std::vector<int> row;
+            for (const char c : line)
+            {
+                // if pixel is shaded, set to 1; blank pixels are -1
+                switch (c)
+                {
+                case '\r':
+                    continue;
+                case '@':
+                    row.emplace_back(1);
+                    break;
+                case '.':
+                    row.emplace_back(-1);
+                    break;
+                default:
+                    throw std::logic_error("The drawing has an invalid format. Please fix the drawing in accordance to specifications.");
+                }
+            }
+            singleDrawing.push_back(row);
+        }
+        return singleDrawing;
+    }
+}
